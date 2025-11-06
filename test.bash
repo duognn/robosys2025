e6ccdb11e3028665027705da8f5f4a957cd33b07
duong <duong.com>
2025-11-06 13:03:30 +0900

    test.yml

--- test.bash
+++ test.bash
@@ -2,14 +2,14 @@
 # SPDX-FileCopyrightText: 2025 duogg　　　　　
 # SPDX-License-Identifier: GPL-3.0-only
 
+ng () {
+	echo ${1} 1st miss
+	res=1
+}
+res=0
+
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
-  　 
-out=$(echo あ | ./plus)    
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"          out=$(echo | ./plus)             
-[ "$?" = 1 ]      || ng "$LINENO" 
-[ "${out}" = "" ] || ng "$LINENO"
-  　 
-[ "$res" = 0 ] && echo OK
+
+[ "${res}" = 0 ] && echo OK
 exit $res
